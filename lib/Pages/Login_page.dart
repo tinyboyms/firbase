763c9a733805fadbff5b8cc63217269ec3369155
tinyboyms <sej9662@gmail.com>
2023-03-26 12:39:03 +0530

    init commit

---
+++ lib/Pages/Login_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        body:Column(
+          children: [
+            ElevatedButton(
+              onPressed: (){
+              },
+              child: Text('Button'),
+              style: ButtonStyle(),
+            ),
+            // ElevatedButton(onPressed: (){},child: Text("LOGIN"),),
+          ],
+        )
+
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:firbase/Pages/Login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+
+
